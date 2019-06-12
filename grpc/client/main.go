b851033c2ce6a8cae2c1a4612afee9b945d74964
DuanJiaNing <aimeimeits@gmail.com>
2019-06-12 09:08:45 +0800

    grpc authentication

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+	"net"
+	"net/http"
 	"time"
 
 	"google.golang.org/grpc"
@@ -10,15 +12,34 @@ import (
 	pb "GoStuff/grpc/services"
 )
 
+type tokenAuth struct {
+	token string
+}
+
+// Return value is mapped to request headers.
+func (t tokenAuth) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": "Bearer " + t.token,
+	}, nil
+}
+
+func (tokenAuth) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
 	conn, err := grpc.Dial("localhost:3332", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+	send(context.Background(), conn)
+}
+
+func send(ctx context.Context, conn *grpc.ClientConn) {
 	c := pb.NewAccountClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
 
 	r, err := c.AddAccount(ctx, &pb.AccountRequest{
@@ -35,4 +56,22 @@ func main() {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("After get account: %s", r)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	token := "test"
+
+	conn, err := grpc.DialContext(ctx, net.JoinHostPort("localhost", "3332"),
+		//grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(insecure.CertPool, "")),
+		grpc.WithPerRPCCredentials(tokenAuth{
+			token: token,
+		}),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	send(ctx, conn)
+
 }
