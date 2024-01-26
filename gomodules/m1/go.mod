0ca1ae3431bbb1225c79f3fe02ccdcd111b70755
段佳宁 <aimeimeits@gmail.com>
2024-01-26 14:25:22 +0800

    1

--- gomodules/m1/go.mod
+++ gomodules/m1/go.mod
@@ -1,7 +1,17 @@
 module hello
 
+go 1.19
+
+require github.com/labstack/echo v3.3.10+incompatible
+
 require (
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.9 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
+	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed // indirect
+	golang.org/x/text v0.3.0 // indirect
 )
--- sort/main.go
+++ sort/main.go
@@ -1,60 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-type strz struct {
-	dst int
-	dt  int
-	dsn string
-
-	st int
-	dk string
-}
-
-type byKey []*strz
-
-func (b byKey) Len() int {
-	return len(b)
-}
-
-func (b byKey) Less(i, j int) bool {
-	o1 := b[i]
-	o2 := b[j]
-
-	// 排序优先级: dsn, dt, dst, dk, st
-	return o1.dsn < o2.dsn ||
-		(o1.dsn == o2.dsn && o1.dt < o2.dt) ||
-		(o1.dsn == o2.dsn && o1.dt == o2.dt && o1.dst < o2.dst) ||
-		(o1.dsn == o2.dsn && o1.dt == o2.dt && o1.dst == o2.dst && o1.dk < o2.dk) ||
-		(o1.dsn == o2.dsn && o1.dt == o2.dt && o1.dst == o2.dst && o1.dk == o2.dk && o1.st < o2.st)
-
-}
-
-func (b byKey) Swap(i, j int) {
-	b[i], b[j] = b[j], b[i]
-}
+import "fmt"
 
 func main() {
-
-	ds := []*strz{
-		{0, 20, "a", 100, "h",},
-		{-1, 20, "a", 100, "h",},
-		{-1, 20, "a", 101, "h",},
-		{1, 20, "a", 100, "h",},
-		{7, 21, "f", 101, "h",},
-		{6, 22, "d", 102, "k",},
-		{2, 29, "b", 103, "j",},
-		{2, 39, "b", 103, "j",},
-		{3, 28, "a", 104, "i",},
-		{1, 26, "c", 107, "l",},
+	nums := []int{3, 4, 1, 2, 8, 5}
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println(nums) // [1 2 3 4 5 8]
+}
+func quickSort(arr []int, l, r int) []int {
+	if l >= r {
+		return arr
 	}
 
-	sort.Sort(byKey(ds))
-	for _, d := range ds {
-		fmt.Printf("%+v\n", d)
-	}
+	var p int
+	arr, p = partition(arr, l, r)
+	quickSort(arr, l, p-1)
+	quickSort(arr, p+1, r)
+	return arr
+}
 
+func partition(arr []int, l, r int) ([]int, int) {
+	pivot := arr[r]
+	i := l
+	for j := l; j < r; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[r] = arr[r], arr[i]
+	return arr, i
 }
